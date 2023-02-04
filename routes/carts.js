44f1c52e504bac4d332dc0f1a9924166f1153e65
MOZZ\admin <yeong0319@naver.com>
2023-02-04 16:11:05 +0900

    feat: 동영상까지 기능구현 완료

---
+++ routes/carts.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+
+const Goods = require("../schemas/goods.js");
+const Cart = require("../schemas/cart.js");
+
+
+router.get("/carts", async (req, res) => {
+  const carts = await Cart.find({});
+  
+  const goodsIds = carts.map((cart) => {return cart.goodsId});
+
+  const goods = await Goods.find({ goodsId: goodsIds });
+
+  const results = carts.map((cart) => {
+		return {
+			quantity: cart.quantity,
+			goods: goods.find((item) => item.goodsId === cart.goodsId)
+		};
+  });
+
+  res.json({
+    carts: results,
+  });
+});
+
+module.exports = router;
